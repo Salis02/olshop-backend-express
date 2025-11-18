b0038ab5de93304d2bcd063f76f049c8c7c83855
Salis02 <nizarsalis@gmail.com>
2025-11-18 11:46:09 +0700

    Implement review controller and routes for creating and retrieving reviews

---
+++ src/controllers/review.controller.js
@@ -0,0 +1,26 @@
+const reviewService = require('../services/review.service')
+const { success, error } = require('../utils/response')
+
+const createReview = async (req, res) => {
+    try {
+        const review = await reviewService.createReview(req.user.uuid, req.body)
+        return success(res, review, 'Reviews created successfullly', 201)
+    } catch (err) {
+        return error(res, err.message)
+    }
+
+}
+
+const getReview = async (req, res) => {
+    try {
+        const review = await reviewService.getProductReviews(req.params.product_id)
+        return success(res, review, 'Reviews retrieved successfullly', 200)
+    } catch (err) {
+        return error(res, err.message)
+    }
+}
+
+module.exports = {
+    createReview,
+    getReview
+}
---
+++ src/routes/review.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const authMiddleware = require('../middlewares/auth.middleware')
+const reviewController = require('../controllers/review.controller')
+
+router.post('/', authMiddleware, reviewController.createReview)
+router.get('/', authMiddleware, reviewController.getReview)
+
+module.exports = router
