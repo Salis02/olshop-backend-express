9d5630a12822bec82fcecee320cc8b17a1decec3
Salis02 <nizarsalis@gmail.com>
2025-11-20 12:52:34 +0700

    Enhance image upload middleware: add file filter and size limit for validation

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,6 +1,7 @@
 const multer = require('multer')
+const path = require('path')
 
-// Set uploaded image locally (example)
+// Set uploaded image folder to locally (example)
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/products');
@@ -10,6 +11,24 @@ const storage = multer.diskStorage({
     }
 })
 
+// File extension validation
+const fileFilter = (req, file, cb) => {
+    const allowed = ['images/jpeg', 'images/jpg', 'images/png', 'image/webp'];
+
+    if (!allowed.includes(file.mimetype)) {
+        return cb(new Error('File type not allowed'), false);
+    }
+
+    cb(null, true)
+};
+
+//Limit max 5mb
+const limit = {
+    fileSize: 5 * 1024 * 1024,
+};
+
 module.exports = {
-    storage
+    storage,
+    fileFilter,
+    limit
 }
