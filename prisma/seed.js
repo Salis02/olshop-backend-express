e439fb4b0079e3b4cbe1097a5f3a98b09963d9b3
Salis02 <nizarsalis@gmail.com>
2025-12-16 11:10:57 +0700

    feat: add seller and customer users with respective roles in database seeding

--- prisma/seed.js
+++ prisma/seed.js
@@ -22,6 +22,13 @@ async function main() {
         },
     })
 
+    const sellerRole = await prisma.role.create({
+        data: {
+            name: 'SELLER',
+            description: 'Seller with have some access'
+        }
+    })
+
     const userRole = await prisma.role.create({
         data: {
             name: 'USER',
@@ -38,6 +45,28 @@ async function main() {
             role_id: adminRole.id,
             name: 'Super Admin',
             email: 'admin@example.com',
+            password: hashedPassword,
+            phone: '081227209872',
+            email_verified_at: new Date()
+        }
+    })
+
+    // Seller User
+    const sellerUser = await prisma.user.create({
+        data: {
+            role_id: sellerRole.id,
+            name: 'Seller reguler',
+            email: 'seller@example.com',
+            password: hashedPassword,
+            phone: '082325361729',
+            email_verified_at: new Date()
+        }
+    })
+    const customerUser = await prisma.user.create({
+        data: {
+            role_id: userRole.id,
+            name: 'User',
+            email: 'user@example.com',
             password: hashedPassword,
             phone: '081227209872',
             email_verified_at: new Date()
