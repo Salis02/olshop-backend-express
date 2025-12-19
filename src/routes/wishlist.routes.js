bd4b537729ad5b5ca06ea756bbf9ad78a0e243f0
Salis02 <nizarsalis@gmail.com>
2025-12-19 22:02:03 +0700

    refactor: add allowRole for wishlist routes

--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middlewares/auth.middleware')
 const wishlistController = require('../controllers/wishlist.controller')
+const allowRoles = require('../middlewares/role.middleware')
 
-router.get('/', authMiddleware, wishlistController.getUserWishlist)
-router.post('/', authMiddleware, wishlistController.addWishlist)
-router.delete('/:product_id', authMiddleware, wishlistController.removeWishlist)
+router.get('/', authMiddleware, allowRoles('USER'), wishlistController.getUserWishlist)
+router.post('/', authMiddleware, allowRoles('USER'), wishlistController.addWishlist)
+router.delete('/:product_id', authMiddleware, allowRoles('USER'), wishlistController.removeWishlist)
 
 module.exports = router
