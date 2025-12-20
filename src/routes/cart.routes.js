4646128f73fdfd0a73cc6f19b55ff19019866f04
Salis02 <nizarsalis@gmail.com>
2025-12-20 21:40:40 +0700

    feat: role checking in cart routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth.middleware');
 const cartController = require('../controllers/cart.controller');
+const allowRoles = require('../middlewares/role.middleware')
 
-router.get('/', authMiddleware, cartController.getCart);
-router.post('/', authMiddleware, cartController.addItem);
-router.put('/:id', authMiddleware, cartController.updateItem);
-router.delete('/:id', authMiddleware, cartController.removeItem);
-router.delete('/', authMiddleware, cartController.clearCart);
+router.get('/', authMiddleware, allowRoles('USER'), cartController.getCart);
+router.post('/', authMiddleware, allowRoles('USER'), cartController.addItem);
+router.put('/:id', authMiddleware, allowRoles('USER'), cartController.updateItem);
+router.delete('/:id', authMiddleware, allowRoles('USER'), cartController.removeItem);
+router.delete('/', authMiddleware, allowRoles('USER'), cartController.clearCart);
 
 module.exports = router;
