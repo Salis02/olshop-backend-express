5958ed395667594162db28813d574111adc93cb1
Salis02 <nizarsalis@gmail.com>
2026-01-04 12:19:58 +0700

    feat: Add API routes for order and cart management.

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const orderController = require('../controllers/order.controller');
 const allowRoles = require('../middlewares/role.middleware');
 
-router.post('/', authMiddleware, allowRoles('USER'), orderController.createOrder);
+router.post('/', authMiddleware, allowRoles('USER', 'SELLER'), orderController.createOrder);
 router.get('/', authMiddleware, allowRoles('USER', 'SELLER', 'ADMIN'), orderController.getOrders);
 router.get('/:uuid', authMiddleware, allowRoles('USER', 'SELLER', 'ADMIN'), orderController.getOrderDetails);
 
